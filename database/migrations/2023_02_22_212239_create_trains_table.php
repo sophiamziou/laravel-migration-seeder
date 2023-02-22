d1ef7f07591682b754b2fc9acbe8826f00a8a0ba
sophiamziou <sofiamziou@gmail.com>
2023-02-22 22:37:57 +0100

    table created

--- database/migrations/2023_02_22_212239_create_trains_table.php
+++ database/migrations/2023_02_22_212239_create_trains_table.php
@@ -15,6 +15,15 @@ return new class extends Migration
     {
         Schema::create('trains', function (Blueprint $table) {
             $table->id();
+            $table->string('azienda', 30);
+            $table->string('stazione_di_partenza', 20);
+            $table->string('stazione_di_arrivo', 20);
+            $table->string('orario_di_partenza', 10);
+            $table->string('orario_di_arrivo', 10);
+            $table->string('codice_treno', 10);
+            $table->smallInteger('numero_carrozze')->unsigned();
+            $table->boolean('in_orario');
+            $table->boolean('cancellato');
             $table->timestamps();
         });
     }
